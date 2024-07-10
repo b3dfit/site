90988432368f8fe58ab8480117f1c27fcf8aaa48
Weslley Alves <weslley@wezo.com.br>
2024-07-09 21:56:38 -0300

    add debug category

--- wp-content/themes/kupodigitalb3d/parts/header.php
+++ wp-content/themes/kupodigitalb3d/parts/header.php
@@ -63,6 +63,9 @@
                             'orderby' => 'name',
                             'order' => 'ASC'
                         ));
+
+                        var_dump($categories);die;
+
                         foreach ($categories as $category) {
                             $category_link = get_category_link($category->term_id);
                             $category_name = $category->name;
