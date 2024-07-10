967bf7784d410943a78e06dcf0e972d93dfa31fd
Weslley Alves <weslley@wezo.com.br>
2024-07-10 20:35:19 -0300

    add page store list

--- wp-content/themes/kupodigitalb3d/archive-loja.php
+++ wp-content/themes/kupodigitalb3d/archive-loja.php
@@ -3,66 +3,74 @@
 
 
 <main id="content">
-    <div class="max-w-5xl flex  px-4 xl:px-0 py-10 mx-auto" id="category-store"></div>
+    <div class="max-w-5xl px-4 xl:px-0 py-10 mx-auto  text-zinc-950 dark:text-white" id="category-store">
+
+
+        <!-- Title -->
+        <div class="w-4/5 block mx-auto">
+            <h2 class="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">Nossas Marcas e Lojas Parceiras
+                Confiáveis</h2>
+            <p class="mt-1 mb-3">Explore nossa seleção de marcas e lojas parceiras
+                de
+                confiança, onde você encontrará os melhores produtos e serviços para aprimorar sua experiência
+                esportiva.</p>
+            <p class="mt-1 mb-3">Selecionamos cuidadosamente cada parceiro para garantir
+                que você tenha acesso a produtos de
+                alta qualidade e ofertas exclusivas.</p>
+            <p class="mt-1 mb-3">Descubra onde comprar seus tênis de performance e
+                acessórios
+                esportivos com total segurança e confiança.</p>
+        </div>
+        <!-- End Title -->
+
+
+        <!-- Card Blog -->
+        <div class="max-w-5xl px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+            <!-- Grid -->
+            <div class="grid lg:grid-cols-2 lg:gap-y-16 gap-10">
 
 
 
 
-    <!-- Card Blog -->
-    <div class="max-w-5xl px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto text-zinc-950 dark:text-white">
-        <!-- Grid -->
-        <div class="grid lg:grid-cols-2 lg:gap-y-16 gap-10">
 
+                <?php
+                $args = array(
+                    'post_type' => 'loja',
+                    'posts_per_page' => 50,
+                    'orderby' => 'title',
+                    'order' => 'ASC',
+                    // 'meta_query' => array(
+                    //     array(
+                    //         'key' => 'store_logosvg',
+                    //         'compare' => 'EXISTS'
+                    //     ),
+                    //     array(
+                    //         'key' => 'store_logosvg',
+                    //         'value' => '',
+                    //         'compare' => '!='
+                    //     )
+                    // )
+                );
 
-
-
-
-            <?php
-            $args = array(
-                'post_type' => 'loja',
-                'posts_per_page' => 50,
-                'orderby' => 'title',
-                'order' => 'ASC',
-                // 'meta_query' => array(
-                //     array(
-                //         'key' => 'store_logosvg',
-                //         'compare' => 'EXISTS'
-                //     ),
-                //     array(
-                //         'key' => 'store_logosvg',
-                //         'value' => '',
-                //         'compare' => '!='
-                //     )
-                // )
-            );
-
-            $loja_query = new WP_Query($args);
-            if ($loja_query->have_posts()) :
-                while ($loja_query->have_posts()) :
-                    $loja_query->the_post();
-                    $link = get_permalink($post->ID);
-                    $svg = getValueCustomPostTypeReview($post->ID, 'logosvg', 'store');
-                    $name = get_the_title($post->ID);
-                    $image = getValueCustomPostTypeReview($post->ID, 'logo', 'store');
-                    $svg = getValueCustomPostTypeReview($post->ID, 'logosvg', 'store');
-                    $description = get_the_excerpt($post->ID);
-                    $html = <<<HTML
+                $loja_query = new WP_Query($args);
+                if ($loja_query->have_posts()) :
+                    while ($loja_query->have_posts()) :
+                        $loja_query->the_post();
+                        $link = get_permalink($post->ID);
+                        $svg = getValueCustomPostTypeReview($post->ID, 'logosvg', 'store');
+                        $name = get_the_title($post->ID);
+                        $image = getValueCustomPostTypeReview($post->ID, 'logo', 'store');
+                        $svg = getValueCustomPostTypeReview($post->ID, 'logosvg', 'store');
+                        $description = get_the_excerpt($post->ID);
+                        $html = <<<HTML
                     <!-- Card -->
                     <a class="group rounded-xl overflow-hidden shadow-md px-5" href="{$link}">
                         <div class="flex">
                             <div class="flex-shrink-0 relative rounded-xl overflow-hidden w-full sm:w-56 h-44">
-                            
                                 <div class="flex group-hover:scale-105 transition-transform duration-500 ease-in-out size-full absolute top-0 start-0 object-cover rounded-xl fill-black bg-white dark:bg-zinc-950  items-center svg-page-stores">
                                     {$svg}
                                 </div>
-                                
-                                <!--img class="group-hover:scale-105 transition-transform duration-500 ease-in-out size-full absolute top-0 start-0 object-cover rounded-xl"
-                                        src="{$image}"
-                                        
-                                        alt="Image Description"-->
-
                             </div>
-
                             <div class="grow mt-4 sm:mt-0 sm:ms-6 px-4 sm:px-0 mb-2">
                                 <h3
                                     class="text-xl font-semibold  group-hover:text-gray-600  dark:group-hover:text-white">
@@ -85,21 +93,16 @@
                     </a>
                     <!-- End Card -->
                     HTML;
-                    echo $html;
+                        echo $html;
+                    endwhile;
+                    wp_reset_postdata();
+                endif;
+                ?>
 
-
-
-
-
-                endwhile;
-                wp_reset_postdata();
-            endif;
-            ?>
-
+            </div>
+            <!-- End Grid -->
         </div>
-        <!-- End Grid -->
-    </div>
-    <!-- End Card Blog -->
+        <!-- End Card Blog -->
 
 
 
