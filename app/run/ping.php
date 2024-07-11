87d0bc5ee659dde077128d0051e93707d3f68d18
Weslley Alves <weslley@wezo.com.br>
2024-07-11 02:15:28 -0300

    Update ping.php

--- app/run/ping.php
+++ app/run/ping.php
@@ -1,5 +1,12 @@
 <?php
-// ping.php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    exit(0);
+}
+
 $data = json_decode(file_get_contents('php://input'), true);
 
 if ($data) {
@@ -18,3 +25,4 @@ if ($data) {
 } else {
     echo 'Nenhum dado recebido';
 }
+?>
