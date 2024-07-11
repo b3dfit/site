6fcb7dbdde800d77f65f919ebce49c3f95878ebd
Weslley Alves <weslley@wezo.com.br>
2024-07-10 21:16:19 -0300

    add page store list

--- wp-content/themes/kupodigitalb3d/archive-loja.php
+++ wp-content/themes/kupodigitalb3d/archive-loja.php
@@ -3,12 +3,12 @@
 
 
 <main id="content">
-    <div class="max-w-5xl px-4 xl:px-0 py-10 mx-auto  text-zinc-950 dark:text-white" id="category-store">
+    <div class="max-w-5xl px-4 py-10 mx-auto  text-zinc-950 dark:text-white" id="category-store">
 
 
         <!-- Title -->
         <div class="w-4/5 block mx-auto">
-            <h2 class="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">Nossas Marcas e Lojas Parceiras
+            <h2 class="text-2xl font-bold">Nossas Marcas e Lojas Parceiras
                 Confiáveis</h2>
             <p class="mt-1 mb-3">Explore nossa seleção de marcas e lojas parceiras
                 de
@@ -65,18 +65,17 @@
                         $html = <<<HTML
                     <!-- Card -->
                     <a class="group rounded-xl overflow-hidden shadow-md px-1" href="{$link}">
-                        <div class="flex">
-                            <div class="flex-shrink-0 relative rounded-xl overflow-hidden w-full sm:w-56 h-44">
-                                <div class="flex group-hover:scale-105 transition-transform duration-500 ease-in-out size-full absolute top-0 start-0 object-cover rounded-xl fill-black bg-white dark:bg-zinc-950  items-center svg-page-stores">
+                        <div class="flex flex-col">
+                            <div class="flex-shrink-0 relative rounded-xl w-full h-44">
+                                <div class="flex group-hover:scale-105 transition-transform duration-500 ease-in-out size-full absolute top-0 start-0 object-cover rounded-xl fill-black bg-white dark:bg-zinc-950 items-center svg-page-stores">
                                     {$svg}
                                 </div>
                             </div>
-                            <div class="grow mt-4 sm:mt-0 sm:ms-6 px-4 sm:px-0 mb-2">
-                                <h3
-                                    class="text-xl font-semibold  group-hover:text-gray-600  dark:group-hover:text-white">
+                            <div class="grow mt-4 px-4 mb-2 text-zinc-950 dark:text-white">
+                                <h3 class="text-xl font-semibold text-zinc-950">
                                     {$name}
                                 </h3>
-                                <p class="mt-3 ">
+                                <p class="mt-3">
                                     {$description}
                                 </p>
                                 <p
