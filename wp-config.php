eec1174e08151abfe7ca8caf4fecd867e751633a
Weslley Alves <weslley@wezo.com.br>
2024-07-14 16:27:32 -0300

    fix wp-config

--- wp-config.php
+++ wp-config.php
@@ -44,23 +44,23 @@ if ($dotenv['YNV_ENV'] === 'DEV') :
     error_reporting(E_ERROR | E_PARSE);
 endif;
 
-define( 'DB_CHARSET', 'utf8mb4' );
+define('DB_CHARSET', 'utf8mb4');
 
-define( 'DB_COLLATE', '' );
+define('DB_COLLATE', '');
 
-define( 'AUTH_KEY',         'uXC=aH(fR(x!-)T{m~+c*:7hL~. m/(=q_a,td~uMOm t*l.jd^1`hFI>O%6k}YT' );
-define( 'SECURE_AUTH_KEY',  'C_TY^p,F9)=gC1}~+=8hW,tH[f=s!XXu0xoL4+.hmDrHs/l$@23<*[dw7*J*D<>.' );
-define( 'LOGGED_IN_KEY',    '7Fu*Ia7eR)BN/`W4*ht|%7{dK%YJEP]U[7qlN9~oe(L;}+dm46MTLuQ8AP}20-qX' );
-define( 'NONCE_KEY',        'Gd#;7m,v{mYi>;$dL(jC>]_V<;A~vHB~RG&/}<4I/y)zn`PupR?n,mBh0;1w$.{s' );
-define( 'AUTH_SALT',        ';V{{pp,es?vQOaFs]KJ4fhT%Zz.*);e7-f2x=5X]G-9i:W|O?W,btX[ue}V;[hI{' );
-define( 'SECURE_AUTH_SALT', ']cAK(6WP2d1t;pSSQ PD{:)K6JA2TZ0x|/I^KeN+mv;XpES/.VNONrN=QCqs@_M-' );
-define( 'LOGGED_IN_SALT',   ';z`|<Gy,g[zocy1U-e Pw}a*{(Mt a0t{Iwd0c^>)]]$x/#j|W+a0 WXx2Sb0(:;' );
-define( 'NONCE_SALT',       '7ot5Sc#Q,VAXD,i,4 qg4|-YT}qyN^!$D(QBcU#=[SEdak?a@sA?6^*7cLz+A5:G' );
+define('AUTH_KEY', $dotenv['AUTH_KEY']);
+define('SECURE_AUTH_KEY', $dotenv['SECURE_AUTH_KEY']);
+define('LOGGED_IN_KEY', $dotenv['LOGGED_IN_KEY']);
+define('NONCE_KEY', $dotenv['NONCE_KEY']);
+define('AUTH_SALT', $dotenv['AUTH_SALT']);
+define('SECURE_AUTH_SALT', $dotenv['SECURE_AUTH_SALT']);
+define('LOGGED_IN_SALT', $dotenv['LOGGED_IN_SALT']);
+define('NONCE_SALT', $dotenv['NONCE_SALT']);
 
-$table_prefix = 'b3d';
+$table_prefix = $dotenv['DB_TABLE_PREFIX'];
 
-if ( ! defined( 'ABSPATH' ) ) {
-	define( 'ABSPATH', __DIR__ . '/' );
+if (! defined('ABSPATH')) {
+    define('ABSPATH', __DIR__ . '/');
 }
 
 require_once ABSPATH . 'wp-settings.php';
