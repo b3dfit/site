96d942d324c32669f39dc7a4fe196c0d9d90aaa2
Weslley Alves <weslley@wezo.com.br>
2024-07-14 17:16:11 -0300

    add template home

---
+++ wp-content/themes/kupodigitalb3d/parts/hero.php
@@ -0,0 +1,17 @@
+<div class="dark:bg-zinc-950">
+    <div class="max-w-5xl mx-auto px-4 xl:px-0 pt-24 lg:pt-24 pb-24">
+        <h1 class="font-semibold dark:text-white text-5xl md:text-6xl">
+            <span class="text-white bg-zinc-950 dark:text-zinc-950 dark:bg-white rounded-lg mr-2">&nbsp;Building<span
+                    class="text-lime-400 dark:text-lime-400">³</span> Discipline </span> o primeiro passo rumo ao
+            seu novo eu!
+        </h1>
+        <div class="max-w-4xl">
+            <p class="mt-5 dark:text-neutral-400 text-lg">
+                <?php
+                the_content();
+                echo '<p class="mt-5">A plataforma definitiva para entusiastas de corrida e atividades físicas. Aqui você terá acesso a recomendações especializadas de tênis de performance, projetadas para melhorar seu desempenho e conforto durante cada passo!</p>';
+                ?>
+            </p>
+        </div>
+    </div>
+</div>
---
+++ wp-content/themes/kupodigitalb3d/template-home.php
@@ -0,0 +1,43 @@
+<?php
+/*
+Template Name: Home
+*/
+get_header();
+?>
+
+
+
+<!-- ========== MAIN CONTENT ========== -->
+<main id="content">
+
+    <!-- Hero -->
+    <?php get_template_part('parts/hero'); ?>
+    <!-- End Hero -->
+
+    <!-- Clients -->
+    <?php get_template_part('parts/stores'); ?>
+    <!-- End Clients -->
+
+    <!-- Case Stories -->
+    <?php get_template_part('parts/stats'); ?>
+    <!-- End Case Stories -->
+
+    <!-- Testimonials -->
+    <?php get_template_part('parts/testimonials'); ?>
+    <!-- End Testimonials -->
+
+    <!-- Stats -->
+    <?php get_template_part('parts/stats-platform'); ?>
+    <!-- End Stats -->
+
+    <!-- Approach -->
+    <?php get_template_part('parts/approach'); ?>
+    <!-- End Approach -->
+
+    <!-- Contact -->
+    <?php //get_template_part('parts/contact'); ?>
+    <!-- End Contact -->
+
+</main>
+<!-- ========== END MAIN CONTENT ========== -->
+<?php get_footer(); ?>
