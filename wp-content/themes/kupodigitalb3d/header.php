6e7b02a8cc1b9e772a45b23a17434b0aa19c5f38
Weslley Alves <weslley@wezo.com.br>
2024-07-16 00:30:27 -0300

    fix header

--- wp-content/themes/kupodigitalb3d/header.php
+++ wp-content/themes/kupodigitalb3d/header.php
@@ -2,52 +2,33 @@
 <html lang="pt-BR" class="h-full">
 
 <head>
-  <!-- Required Meta Tags Always Come First -->
+
   <meta charset="utf-8">
   <meta name="robots" content="max-snippet:-1, max-image-preview:large, max-video-preview:-1">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-
-
-
-  <!-- Meta Tags para SEO -->
-  <meta name="description"
-    content="A plataforma para entusiastas de atividades físicas. Aqui você terá acesso a recomendações especializadas de tênis de performance, aplicativos e relógios inteligentes, roupas com tecidos especiais e acessórios, projetadas para melhorar seu desempenho e conforto durante cada passo!">
-  <meta name="keywords"
-    content="tênis de performance, recomendações de tênis, aplicativos fitness, relógios inteligentes, roupas esportivas, acessórios para esportes">
-  <meta name="author" content="Plataforma de Recomendações de Tênis">
-
-
-  <!-- Meta Tags para Contato e Localização -->
-  <meta name="contact:phone_number" content="+55-11-99897-0080">
-  <meta name="contact:email" content="hi@b3d.com.br">
-  <meta name="contact:available_language" content="Portuguese">
-  <meta name="geo.region" content="BR">
-  <meta name="geo.placename" content="Brasil">
-
-
-
-  <meta name="twitter:site" content="@wezo">
-  <meta name="twitter:creator" content="@wezo">
-  <meta name="twitter:card" content="summary_large_image">
-  <meta name="twitter:title" content="<?php the_title(); ?>">
-  <meta name="twitter:description"
-    content="Acesse recomendações especializadas de tênis de performance, aplicativos, relógios inteligentes, roupas e acessórios para melhorar seu desempenho e conforto nas atividades físicas.">
-  <meta name="twitter:image" content="https://www.b3d.com.br/files/dist/images/og-image-dark.png">
-
-  <link rel="apple-touch-icon" sizes="180x180" href="/files/dist/images/apple-touch-icon.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="/files/dist/images/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="/files/dist/images/favicon-16x16.png">
-  <link rel="manifest" href="/files/dist/images/site.webmanifest">
+  
+  
 
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
-  <!-- CSS HS -->
   <link rel="stylesheet" href="https://preline.co/assets/css/main.min.css">
 
   <meta name="lomadee-verification" content="23361002" />
   <meta name="facebook-domain-verification" content="3n6rlvsi2g16v784gpc20f74hf5m17" />
   <meta property="fb:app_id" content="717857133771382" />
+  
   <?php wp_head(); ?>
+
+  <meta name="contact:phone_number" content="+55-11-99897-0080">
+  <meta name="contact:email" content="hi@b3d.com.br">
+  <meta name="contact:available_language" content="Portuguese">
+  <meta name="geo.region" content="BR">
+  <meta name="geo.placename" content="Brasil">
+  <link rel="apple-touch-icon" sizes="180x180" href="/files/dist/images/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/files/dist/images/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/files/dist/images/favicon-16x16.png">
+  <link rel="manifest" href="/files/dist/images/site.webmanifest">
+
 </head>
 
 
