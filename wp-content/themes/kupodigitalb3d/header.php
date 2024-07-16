917782936294c648d139e7f87f98dc6a3e51ae80
Weslley Alves <weslley@wezo.com.br>
2024-07-16 12:38:39 -0300

    Atualizar o header.php

--- wp-content/themes/kupodigitalb3d/header.php
+++ wp-content/themes/kupodigitalb3d/header.php
@@ -29,10 +29,24 @@
   <link rel="icon" type="image/png" sizes="16x16" href="/files/dist/images/favicon-16x16.png">
   <link rel="manifest" href="/files/dist/images/site.webmanifest">
 
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NCBBTV2V');</script>
+<!-- End Google Tag Manager -->
+
 </head>
 
 
 <body class="dark:bg-zinc-950">
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NCBBTV2V"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
   <?php get_template_part('parts/header'); ?>
 
