dc7fb5108eae7982788996319203b97143f48f08
Weslley Alves <weslley@wezo.com.br>
2024-07-27 20:46:11 -0300

    update load fonts

--- wp-content/themes/kupodigitalb3d/header.php
+++ wp-content/themes/kupodigitalb3d/header.php
@@ -6,13 +6,15 @@
   <meta name="robots" content="max-snippet:-1, max-image-preview:large, max-video-preview:-1">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
+  
   
   <meta name="lomadee-verification" content="23361002" />
   <meta name="facebook-domain-verification" content="3n6rlvsi2g16v784gpc20f74hf5m17" />
   <meta property="fb:app_id" content="717857133771382" />
   
   <?php wp_head(); ?>
+
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
 
   <meta name="contact:phone_number" content="+55-11-99897-0080">
   <meta name="contact:email" content="hi@b3d.com.br">
