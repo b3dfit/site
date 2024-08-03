308a36076d495f6401efbceac644fad31d1d367d
Weslley Alves <weslley@wezo.com.br>
2024-08-03 15:47:55 -0300

    fix order food composition

--- wp-content/themes/kupodigitalb3d/single-alimento.php
+++ wp-content/themes/kupodigitalb3d/single-alimento.php
@@ -5,12 +5,12 @@ global $post;
 $food = (new \Review\Repository\Food())->getById($post->ID);
 $numbersInEnglish = ["1" => 'one', "2" => 'two', "3" => 'three', "4" => 'four', "5" => 'five', "6" => 'six', "7" => 'seven', "8" => 'eight', "9" => 'nine', "10" => 'ten', "11" => 'eleven', "12" => 'twelve', "13" => 'thirteen', "14" => 'fourteen', "15" => 'fifteen', "16" => 'sixteen', "17" => 'seventeen', "18" => 'eighteen', "19" => 'nineteen', "20" => 'twenty'];
 $grouped_compositions = [];
-foreach ($food->getComposition() as $composition) {
-    if ($composition->getId() == 'alimento_id') {
-        continue;
-    }
+$compositionArray = array_values(array_filter($food->getComposition(), fn ($obj) => $obj->getId() !== "alimento_id")); // remove form index alimento_id
+foreach ($compositionArray as $composition) {
     $grouped_compositions[$composition->getGroup()][] = $composition;
 }
+ksort($grouped_compositions);
+
 ?>
 
 <main id="content">
