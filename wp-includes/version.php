8725a23acf98ff13b45c8e3d92ff8dd581b88fa9
Weslley Alves <weslley@wezo.com.br>
2024-10-20 12:12:11 -0300

    update core: 6.6.2

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.6.1';
+$wp_version = '6.6.2';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
@@ -45,3 +45,5 @@ $required_php_version = '7.2.24';
  * @global string $required_mysql_version
  */
 $required_mysql_version = '5.5.5';
+
+$wp_local_package = 'pt_BR';
